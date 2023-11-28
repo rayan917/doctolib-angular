68ceb8d1ff46540e6a5558f3ec3a72200cdaa628
Jeremy Weltmann <jeremy.we@hotmail.fr>
2023-11-28 16:39:48 +0100

    reactive forms login register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: [''],
+      password: ['']
+    });
+  }
+
+  login() {
+    this.authService.loginPatient(this.loginForm.value).subscribe((user: any) => {
+      if (user.length === 0) alert('Erreur dans le pseudo ou le mot de passe');
+      this.authService.user = user[0];
+      if (!this.authService.user) return;
+      this.authService.saveUser();
+      this.router.navigate(['/']);
+    }, () => {
+      alert('Erreur dans la requête');
+    });
+  }
 }
